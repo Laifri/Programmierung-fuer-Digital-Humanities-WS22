ebcecc64b3b630c68dec74ec82b6094681464d27
Laifri <117946852+Laifri@users.noreply.github.com>
2022-12-06 23:01:49 +0100

    Abgabe 7 fertig (noch nicht abgegeben)

--- Programmierabgabe 7.py
+++ Programmierabgabe 7.py
@@ -1,7 +1,10 @@
-#def tokenize(satz):
- #   tokens = satz.split(" ")
-  #  new_tokens = []
-
+#Bauen Sie die tokenize-Funktion aus der Vorlesung nach.
+#--Für die folgenden 2 Schritte können Sie gerne die vorherige Abgabe erneut benutzen--
+#Lesen sie die Datei "corona_twitter.txt" ein und speichern Sie die einzelnen Sätze als Liste.
+#Erweitern Sie die tokenize-Funktion um mindestens eine der drei folgenden Möglichkeiten:
+#-Der Tokenizer soll mindestens 2 weitere Sonderzeichen am Wortende erkennen, die in den Sätzen auftauchen.
+#-Der Tokenizer soll mindestens 3 Sonderzeichen am Satzanfang erkennen, die in den Sätzen vorkommen. Zum Beispiel: "(Kansas)" -> ["(", "Kansas", ")"]
+#-Der Tokenizer soll die Abkürzungen n't und 's erkennen und entsprechend abtrennen. Zum Beispiel: "isn't" -> ["is", "n't"]
 def tokenize(satz):
     tokens = satz.split(" ")
     new_tokens = []
@@ -9,47 +12,53 @@ def tokenize(satz):
     for token in tokens:
         if token == "":
             continue #Ignoriert den restlichen Code aus der Schleife und geht direkt in die nächste über
-        # Hat das Token ein Punkt am Ende?
+        #Hat das Token ein Punkt am Ende?
+        sonderzeichen = {"#","@"}
         if token[-1] == ".":
             new_token = token[:-1]
-            punkt_token = token [-1]
+            punkt = token[-1]
             new_tokens.append(new_token)
-            new_tokens.append(punkt_token)
+            new_tokens.append(punkt)
+        elif token[0] in sonderzeichen:
+            new_tokens.append(token[0])
+            new_tokens.append(token[1:])
+        elif token[-1] == "?":
+            new_token = token[:-1]
+            fragezeichen = token[-1]
+            new_tokens.append(new_token)
+            new_tokens.append(fragezeichen)
+        elif token[0] == "(" and token[-1] == ")":
+            new_tokens.append(token[0])
+            new_tokens.append(token[1:-1])
+            new_tokens.append(token[-1])
         elif (token[0] == "("):
+            new_tokens.append(token[0])
+            new_tokens.append(token[1:])
+        elif (token[-1] == ")"):
+            new_tokens.append(token[:-1])
+            new_tokens.append(token[-1])
+        elif (token[0] == "#"):
             new_tokens.append(token[0])
             new_tokens.append(token[1:])
         else:
             new_tokens.append(token)
     return new_tokens
 
-twitter_daten = open("corona_twitter.txt", "r", encoding="utf8").read().split("\n")[:10] #ersten 100 Sätze
+twitter_daten = open("corona_twitter.txt", "r", encoding="utf8").read().split("\n")[:100] #ersten 100 Sätze
 
+#Ermitteln Sie die durchschnittliche Anzahl von Tokens für alle Sätze, verwenden Sie zum Tokenisieren nun die zuvor implementierte tokenize-Funktion.
+def count_tokens(satz):
+    woerter = tokenize(satz)
+    return len(woerter)
+def durchschnitt_tokens(saetze):
+    token_gesamt = 0
+    for satz in saetze:
+        anzahl_token = count_tokens(satz)
+        token_gesamt += anzahl_token
+    return token_gesamt/len(saetze)
+print("Durschnittliche Wortzahl:", durchschnitt_tokens(twitter_daten))
+
+#Testen Sie zum Schluss ihre Abgabe!
 for line in twitter_daten:
     tokens = tokenize(line)
     print(tokens)
-
-
-
-
-
-
-
-#Bauen Sie die tokenize-Funktion aus der Vorlesung nach.
-
-#--Für die folgenden 2 Schritte können Sie gerne die vorherige Abgabe erneut benutzen--
-
-#Lesen sie die Datei "corona_twitter.txt" ein und speichern Sie die einzelnen Sätze als Liste.
-
-#Ermitteln Sie die durchschnittliche Anzahl von Tokens für alle Sätze, verwenden Sie zum Tokenisieren nun die zuvor implementierte tokenize-Funktion.
-
-#---
-
-#Erweitern Sie die tokenize-Funktion um mindestens eine der drei folgenden Möglichkeiten:
-
-#-Der Tokenizer soll mindestens 2 weitere Sonderzeichen am Wortende erkennen, die in den Sätzen auftauchen.
-
-#-Der Tokenizer soll mindestens 3 Sonderzeichen am Satzanfang erkennen, die in den Sätzen vorkommen. Zum Beispiel: "(Kansas)" -> ["(", "Kansas", ")"]
-
-#-Der Tokenizer soll die Abkürzungen n't und 's erkennen und entsprechend abtrennen. Zum Beispiel: "isn't" -> ["is", "n't"]
-
-#Testen Sie zum Schluss ihre Abgabe!
